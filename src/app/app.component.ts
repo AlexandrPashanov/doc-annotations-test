d2c255f5ddfe4e10cf840da0e309e8ab03114a15
Aleksandr Pashanov <kaaronar@mail.ru>
2025-05-14 15:50:23 +0300

    initial app-component layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { TuiIcon, TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot, TuiIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'doc-annotations-test';
+
+  saveChanges(): void {
+    console.log('saveChanges');
+  }
+
+  decreaseZoom(): void {
+    console.log('decreaseZoom');
+  }
+
+  increaseZoom(): void {
+    console.log('increaseZoom');
+  }
 }
