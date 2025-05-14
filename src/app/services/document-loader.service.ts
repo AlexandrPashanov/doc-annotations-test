f184c6a674f5a7574dca7b65ef464f23c7a3d1c7
Aleksandr Pashanov <kaaronar@mail.ru>
2025-05-14 17:58:47 +0300

    added httpClient and remembering last loaded  id

--- src/app/services/document-loader.service.ts
+++ src/app/services/document-loader.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,13 @@ export class DocumentLoaderService {
     pages: { number: number, imageUrl: string }[],
     annotations: any[]
   } | null> = signal(null);
+  loadedId$: WritableSignal<string> = signal('');
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   loadDocument(documentId: string) {
     console.log('documentId', documentId);
+    this.loadedId$.set(documentId);
     this.document$.set(null);
     if (!documentId) return;
     console.log('loading document', documentId);
